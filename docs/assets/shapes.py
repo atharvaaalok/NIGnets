d44ddc7a3b9709938eb4bc5207347339966eb589
atharvaaalok <atharvaaalok@gmail.com>
2025-02-26 09:03:11 -0800

    Add type annotations for shapes.py script.

--- docs/assets/shapes.py
+++ docs/assets/shapes.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def circle(num_pts):
+def circle(num_pts: int) -> torch.Tensor:
     theta = torch.linspace(0, 2 * torch.pi, num_pts)
 
     # Compute x and y coordinates and concatenate to form a matrix
@@ -9,7 +9,7 @@ def circle(num_pts):
     return X
 
 
-def square(num_pts):
+def square(num_pts: int) -> torch.Tensor:
     # Generate points on the unit circle first and then map them to a square
     theta = torch.linspace(0, 2 * torch.pi, num_pts)
 
