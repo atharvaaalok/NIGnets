1700e0716ddddbb0b60cab2a89c210e227043884
atharvaaalok <atharvaaalok@gmail.com>
2025-02-26 08:58:57 -0800

    Add plot_curves function to abstract away plotting curves.

--- docs/assets/utils.py
+++ docs/assets/utils.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 
 
 def automate_training(
@@ -25,3 +26,18 @@ def automate_training(
         if epoch == 0 or (epoch + 1) % print_cost_every == 0:
             num_digits = len(str(epochs))
             print(f'Epoch: [{epoch + 1:{num_digits}}/{epochs}]. Loss: {loss.item():11.6f}')
+
+
+def plot_curves(Xc, Xt):
+    # Get torch tensor to cpu and disable gradient tracking to plot using matplotlib
+    Xc = Xc.detach().cpu()
+    Xt = Xt.detach().cpu()
+    
+    plt.fill(Xt[:, 0], Xt[:, 1], color = "#C9C9F5", alpha = 0.46, label = "Target Curve")
+    plt.fill(Xc[:, 0], Xc[:, 1], color = "#F69E5E", alpha = 0.36, label = "Candidate Curve")
+
+    plt.plot(Xt[:, 0], Xt[:, 1], color = "#000000", linewidth = 2)
+    plt.plot(Xc[:, 0], Xc[:, 1], color = "#000000", linewidth = 2, linestyle = "--")
+
+    plt.axis('equal')
+    plt.show()
